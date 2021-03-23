5deb59f5ca82d3202e4fdd52c5ddc5b5159191dc
Eric BATARSON <eric.batarson@digivia.fr>
2021-03-23 19:39:27 +0100

    [+] Update roles (delete unused ROLE_AUTHOR)

--- src/Security/Roles/Roles.php
+++ src/Security/Roles/Roles.php
@@ -13,14 +13,12 @@ class Roles
     public const ROLE_USER        = 'ROLE_USER';
     public const ROLE_API         = 'ROLE_API';
     public const ROLE_CONTRIBUTOR = 'ROLE_CONTRIBUTOR';
-    public const ROLE_AUTHOR      = 'ROLE_AUTHOR';
     public const ROLE_EDITOR      = 'ROLE_EDITOR';
     public const ROLE_ADMIN       = 'ROLE_ADMIN';
 
     public const ROLE_HIERARCHY = [
         self::ROLE_CONTRIBUTOR => self::ROLE_USER,
-        self::ROLE_AUTHOR      => self::ROLE_CONTRIBUTOR,
-        self::ROLE_EDITOR      => self::ROLE_AUTHOR,
+        self::ROLE_EDITOR      => self::ROLE_CONTRIBUTOR,
         self::ROLE_ADMIN       => [self::ROLE_EDITOR, self::ROLE_API, 'ROLE_ALLOWED_TO_SWITCH'],
     ];
 
@@ -28,7 +26,6 @@ class Roles
         self::ROLE_USER        => 'Utilisateur',
         self::ROLE_API         => 'Accès API',
         self::ROLE_CONTRIBUTOR => 'Contributeur',
-        self::ROLE_AUTHOR      => 'Auteur',
         self::ROLE_EDITOR      => 'Rédacteur en chef',
         self::ROLE_ADMIN       => 'Administrateur',
     ];
