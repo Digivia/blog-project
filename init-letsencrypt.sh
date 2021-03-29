46a15d8d862f09acb72f5587d205948c50a6b86b
Eric BATARSON <eric.batarson@digivia.fr>
2021-03-29 20:10:17 +0200

    [+] Devops : fix lets encrypt certificate script

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -34,7 +34,6 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(test.digivia.fr)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="contact@digivia.fr" # Adding a valid address is strongly recommended
